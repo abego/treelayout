51e4031e181602723ecbb7e0d4e647272d94d541
udo.borkowski@googlemail.com <udo.borkowski@googlemail.com@21e38e68-a4ad-4b45-8e66-8010ec952233>
2011-11-17 11:21:23 +0000

    New: add scripts for creating a release

---
+++ deployRelease.sh
@@ -0,0 +1,4 @@
+mvn -f org.abego.treelayout/pom.xml -Psonatype-oss-release clean deploy
+mvn -f org.abego.treelayout.demo/pom.xml -Psonatype-oss-release clean deploy
+mvn -f org.abego.treelayout.netbeans/pom.xml -Psonatype-oss-release clean deploy
+mvn -f org.abego.treelayout.netbeans.demo/pom.xml -Psonatype-oss-release clean deploy

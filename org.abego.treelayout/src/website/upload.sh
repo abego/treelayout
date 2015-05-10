a2c23e5728369114cc873dab89da4cc25a3b59ab
Udo Borkowski <ub@abego-software.de>
2015-05-10 19:13:45 +0200

    rewrite documentation as HTML website
    - avoid using (proprietary) markup language
    - update content affected by migration to GitHub

---
+++ org.abego.treelayout/src/website/upload.sh
@@ -0,0 +1,1 @@
+rsync -avp --exclude=.DS_Store --exclude=.svn --exclude=upload.sh -e ssh /Users/ub/work/project/sd/lang/java/project/org.abego.treelayout/org.abego.treelayout/src/website/. udoborkowski,treelayout@web.sourceforge.net:htdocs/
